424f69be80f3c852e66b72f7f6dce5504ae5c64f
Camilo <camilochto@gmail.com>
2026-03-17 16:48:32 +0100

    Millorat casProva2

--- Practica2/Exercici3/main.cpp
+++ Practica2/Exercici3/main.cpp
@@ -34,14 +34,71 @@
     A més, així la llista es un objecte més versàtil que em permèt anar enrere, i controlar 
     millor els objectes que hi conté
 */
-
-
-
 #include <stdexcept>
 #include <iostream>
 #include "Position.h"
 #include "LinkedList.h"
 using namespace std;
+
+
+//Demana [s/n] a l'usuari
+//No ignora la resta del line (revisa cada char i espera per [s/S/n/N])
+char demanaSN(string missatge = "Digues"){
+    char usuari = 'x';
+    while (usuari != 's' && usuari != 'n'){
+        cout << missatge << " (s/n) ";
+        cin >> usuari;
+        usuari = tolower(usuari);
+    }
+    return usuari;
+}
+
+//Demana de manera robusta un integer
+int demanaInt(string missatge = "Digues n ∈ ℕ"){
+    bool correcte = false;
+    int usuari;
+    while (!correcte){
+        cout << missatge<< " ";
+        cin >> usuari;
+        correcte = !cin.fail();
+        if (!correcte){
+            cin.clear();
+            cin.ignore(1000,'\n');
+        }
+    }
+    return usuari;
+}
+
+//Demana un string
+string demanaStr(string missatge = "Digues"){
+    cout << missatge << " ";
+    string usuari;
+    cin >> usuari;
+    return usuari;
+}
+
+//Pot llançar exception, tracta d'inserir un string nou en la pos <pos> de llista
+void inserirEnPos(LinkedList<string> &llista, int pos, string nouStr){
+    //Fem els casos de error
+    if (llista.isEmpty())
+        throw out_of_range("La estructura está buida!");
+    if (pos < 0)
+        throw out_of_range("La posició no pot ser negativa!");
+    if (pos >= llista.size())
+        throw out_of_range("La posició es massa gran (max " + to_string(llista.size()-1) + ")");
+
+    //Iterem fins la posició volguda
+    Position<string> it = llista.beginning();
+    for (int j = 0; j< pos; j++){it = ++it;}
+            
+    //Preguntem si es vol afegir abans o despres
+    int eleccio = demanaSN("Inserir abans de la posició?");
+    if (eleccio == 's')
+        llista.insertBefore(it,nouStr);
+    else
+        llista.insertAfter(it,nouStr);
+}
+
 
 void casProva1(){ 
     LinkedList<int> list1;
@@ -91,80 +148,62 @@ void casProva1(){
     list2.deletePosition(pos2);
     list2.print();
 }
-
-char demanaSN(string missatge = "Digues"){
-    char usuari = 'x';
-    while (usuari != 's' && usuari != 'n'){
-        cout << missatge << " (s/n) ";
-        cin >> usuari;
-        usuari = tolower(usuari);
-    }
-    return usuari;
-}
-int demanaInt(string missatge = "Digues n ∈ ℕ"){
-    int usuari = 0;
-    while (usuari == 0){
-        cout << missatge<< " ";
-        if (!(cin >> usuari)){
-            cin.clear();
-            cin.ignore(1000,'\n');
-        }
-    }
-    return usuari;
-}
-string demanaStr(string missatge = "Digues"){
-    cout << missatge << " ";
-    string usuari;
-    cin >> usuari;
-    return usuari;
-}
 void casProva2(){
     const int MAX = demanaInt("Introdueix quantes paraules vols inserir:");
     LinkedList<string> llista;
+    if (MAX < 0)
+        cout << "Negatiu pensat com 0, sense elements" << endl;
 
     for (int i = 0; i < MAX; i++){
+        //Demanem Paraula
         string str = demanaStr("Introdueix una paraula:");
+
+        /* --Preguntes --*/
+        //Inici
         char eleccio = demanaSN("Inserir al principi?");
         if (eleccio == 's'){
             llista.insertBeginning(str);
             continue;
         }
+        //Fi
         eleccio = demanaSN("Inserir al final?");
         if (eleccio == 's'){
             llista.insertEnd(str);
             continue;
         }
 
-        bool correcte = false;
-
+        //Posició intermig (Cas buida)
         if (llista.isEmpty()){
-            correcte = true;
             llista.insertBeginning(str);
+            cout << "--- Com que está buida, només hi inserim ---" << endl;
+            continue;
         }
-        while (!correcte){
+
+        //Posició intermig (bucle que espera una posició valida)
+        bool valid = false;
+        while(!valid){
             try{
-            int pos = demanaInt("A quina posició vols inserir?");
-            Position<string> it = llista.beginning();
-            for (int j = 0; j< pos; j++){it = ++it;}
-            eleccio = demanaSN("Inserir abans de la posició?");
-                if (eleccio == 's')
-                llista.insertBefore(it,str);
-            else
-                llista.insertAfter(it,str);
-            correcte = true;
+                //Demana integer
+                int pos = demanaInt("A quina posició vols inserir?"); 
+                inserirEnPos(llista,pos,str); //Pot llançar exception
+                 //Si no s'en llença, en sortim
+                valid = true;
             }
-            catch(exception& e){
-                cout << "Error: " << e.what()<< endl;
+            catch(out_of_range &e){
+                cout << "EXCEPTION: "<<e.what() << endl;
             }
         }
     }
+
+    //Fem servir el mètode print de LikedList
     cout << "Print(): ";
     llista.print();
 
+    //Iteració demanada
     cout << "Iteració dels elements endavant: "<<endl;
     Position<string> it = llista.beginning();
     for(it; it != llista.end(); it = ++it)
-        cout << *it<< endl;
+        cout << *it << endl;
 }
 
 int main(int argc, char** argv){
